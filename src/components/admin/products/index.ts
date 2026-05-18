fbd63a6efe5aad0f5eee45a33259e92ba607508d
imanniknam <imanvid2334@gmail.com>
2026-05-18 22:50:00 +0330

    Remove duplicate admin product images export

--- src/components/admin/products/index.ts
+++ src/components/admin/products/index.ts
@@ -4,4 +4,3 @@ export { AdminImageDropzone } from "./admin-image-dropzone";
 export { AdminProductsToolbar } from "./admin-products-toolbar";
 export { AdminPagination } from "./admin-pagination";
 export { AdminProductDeleteButton } from "./admin-product-delete-button";
-export { AdminProductImagesField } from "./admin-product-images-field";
